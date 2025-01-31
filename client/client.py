249a2c955b4a45f0582ff7f5ad4a50d41ecd8675
MatthewWestbrook <C25Matthew.Westbrook@afacademy.af.edu>
2025-01-31 09:18:02 -0700

    found img/file error

--- client/client.py
+++ client/client.py
@@ -12,7 +12,7 @@ def get_img_prediction(
     """Send image to server for prediction."""
     # TODO: Replace with code to send image to server
     ipAddress = f"http://{server_ip}:{server_port}{api_path}"
-    files = {'file': open(image_path,'rb')}
+    files = {'img': open(image_path,'rb')}
     r = requests.post(ipAddress,files=files,timeout = 1)
     return r.text
     # we want to post an HTTP request to the server
@@ -25,14 +25,14 @@ def main(server_ip: str, server_port: int) -> None:
     Then display the result to the user.
     """
     # TODO: Replace with prompt to user and call to get_img_prediction
-    ipAddress = sys.argv[1]
-    port = sys.argv[2]
+    # server_ip = sys.argv[1]
+    # server_port = sys.argv[2]
 
     while(input != "exit"):
         img_path = input("Please enter the path to an image: ")
         api_path = "/predict"
         print(f"Using server {server_ip}:{server_port}")
-        text = get_img_prediction(ipAddress,port,api_path,img_path)
+        text = get_img_prediction(server_ip,server_port,api_path,img_path)
         print(f"The result of the prediction is: {text}\n")
 
     print("Goodbye!\n")
