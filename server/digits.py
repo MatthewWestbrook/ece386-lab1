4a021653f275b1b602222ddc78de2aabf094c033
MatthewWestbrook <C25Matthew.Westbrook@afacademy.af.edu>
2025-02-01 13:50:46 -0700

    black formatting

--- server/digits.py
+++ server/digits.py
@@ -14,7 +14,7 @@ the NIST digits dataset.
 from tensorflow.keras.saving import load_model  # type: ignore[import]
 from PIL import Image
 from io import BytesIO
-from typing import Annotated
+from typing import Annotated, List
 from fastapi import FastAPI, File, UploadFile
 
 import numpy as np
@@ -26,9 +26,11 @@ file_object = load_model(model_path)
 # TODO: Create FastAPI App as global variable
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
     return {"message": "Welcome to the server! Nice to see you"}
+
 
 def image_to_np(image_bytes: bytes) -> np.ndarray:
     """Convert image to proper numpy array"""
@@ -36,12 +38,12 @@ def image_to_np(image_bytes: bytes) -> np.ndarray:
     img = Image.open(BytesIO(image_bytes))
     # TODO: convert image to grayscale and resize
     gray_image = img.convert("L")
-    resized_image = gray_image.resize((28,28))
+    resized_image = gray_image.resize((28, 28))
     # TODO: convert image to numpy array of shape model expects
     image_array = np.array(resized_image)
 
     # we were getting some issues with batch size, so I manually added it
-    image_array = image_array.reshape((1, 28, 28)) 
+    image_array = image_array.reshape((1, 28, 28))
 
     return image_array
 
@@ -52,11 +54,12 @@ def image_to_np(image_bytes: bytes) -> np.ndarray:
 #     prediction = file_object.predict(img)
 #     return {"image prediction:": prediction}
 
+
 # TODO: Define predict POST function
 @app.post("/predict")
-async def get_request(img: Annotated[list[bytes], File()]) -> dict:
+async def get_request(img: Annotated[List[bytes], File()]) -> dict:
     np_img = np.vstack([image_to_np(image) for image in img])
-    
+
     print(f"initial batch shape: {np_img.shape}")
     prediction = file_object.predict(np_img)
 
