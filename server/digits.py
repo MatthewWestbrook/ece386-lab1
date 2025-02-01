7bd55721e32ea68ce7ed7b2a0d5084d89a66377c
MatthewWestbrook <C25Matthew.Westbrook@afacademy.af.edu>
2025-02-01 13:22:09 -0700

    Finished

--- server/digits.py
+++ server/digits.py
@@ -1,10 +1,14 @@
 """
-TODO: Insert what this program does here. Should start with
-digits is a FastAPI app that predicts 
-
+TODO: 
 This file should be compliant with Pyright.
 The tensorflow import is ignored with # type: ignore[import]
 because tensorflow doesn't support type hints appropriately.
+
+Digits.py is a FastAPI app that predicts the number contained
+in a 28x28 black and white image. It accepts a POST request 
+from the cleint.py file. It uses the digits.keras model
+to make these predictions. The model was made by training on
+the NIST digits dataset. 
 """
 
 from tensorflow.keras.saving import load_model  # type: ignore[import]
@@ -17,7 +21,7 @@ import numpy as np
 
 model_path: str = "digits.keras"
 # TODO: Open saved Keras model as global variable. NO TYPE HINT REQUIRED!
-file_object = load_model(model_path, "r")
+file_object = load_model(model_path)
 
 # TODO: Create FastAPI App as global variable
 app = FastAPI()
@@ -36,6 +40,9 @@ def image_to_np(image_bytes: bytes) -> np.ndarray:
     # TODO: convert image to numpy array of shape model expects
     image_array = np.array(resized_image)
 
+    # we were getting some issues with batch size, so I manually added it
+    image_array = image_array.reshape((1, 28, 28)) 
+
     return image_array
 
 
@@ -47,6 +54,13 @@ def image_to_np(image_bytes: bytes) -> np.ndarray:
 
 # TODO: Define predict POST function
 @app.post("/predict")
-async def get_request(img: Annotated[list[bytes], File()]):
-    prediction = file_object.predict(img)
-    return {"image prediction:": prediction}
+async def get_request(img: Annotated[list[bytes], File()]) -> dict:
+    np_img = np.vstack([image_to_np(image) for image in img])
+    
+    print(f"initial batch shape: {np_img.shape}")
+    prediction = file_object.predict(np_img)
+
+    # this came from chat gpt - documented
+    predicted_label = np.argmax(prediction, axis=1).tolist()
+
+    return {"image prediction:": predicted_label}
